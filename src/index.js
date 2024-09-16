bcbac868b94f0093dfb887c64775e1beb32b41e5
Samir Khanal <gdssamir@gmail.com>
2024-09-16 09:50:47 +0545

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 // require('dotenv').config({path:'./env'})// takes vairable from env
-// semicolon for cleanup purpose
 
 dotenv.config({
   //config in package.json
   path: "./env",
 });
-connectDB();
-
-// const app = express();
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`); // db connection
-//     app.on("error", (error) => {
-//       console.log("ERROR: ", error);
-//       throw error;
-//     })
-//     app.listen(process.env.PORT,()=>{
-//         console.log(`App is listening on port ${process.env.PORT}`);
-
-//     })
-//   } catch (error) {
-//     console.log("ERROR: ", error);
-//     throw error;
-//   }
-// })();
+connectDB() // returns promise
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    });
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db connection failed !!! ", error);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+// wrapping function for try catch
+export const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+// const handler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req,res,next)// execute the function
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       // for frontend to get error message in json
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
